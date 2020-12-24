39cb23cd83f616acc47e4a876ae73452cec3d8d1
الواثق الحاج <alwathig777@gmail.com>
2020-12-24 18:19:27 +0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+	"name": "Zakat System",
+	"summary": "To manage Zakat ",
+	"description": """ Main activites """,
+	"sequence" : -99,
+	"website" : "firebits.net",
+	"author" : "Mohammed Alwathig",
+	"depends" : ["base"],
+	"category": "Zakat",
+	"application": False,
+	"auto_install": False,
+	"data": ["views.xml"],
+}
---
+++ zakat.py
@@ -0,0 +1,29 @@
+from odoo import models,fields
+
+class Person(models.Model):
+	_name = "zakat.person"
+	name = fields.Char(required=True)
+	national_no = fields.Integer()
+	phone = fields.Char(string="Mobile")
+	birth_date = fields.Date()
+	image = fields.Binary()
+	address = fields.Text(help="Enter Your city")
+	gender = fields.Selection(selection=[("male","Male"), ("female","Female")], default="male")
+	is_deserve = fields.Boolean()
+	orders = fields.One2many("zakat.order","person")
+	masrif_type = fields.Many2many("zakat.type")
+	salary = fields.Float()
+
+
+class Order(models.Model):
+	_name = "zakat.order"
+	topic = fields.Text()
+	date = fields.Date()
+	money = fields.Float(readonly=True)
+	time = fields.Datetime()
+	description = fields.Html()
+	person = fields.Many2one("zakat.person")
+
+class MType(models.Model):
+	_name = "zakat.type"
+	name = fields.Char()
